a69298a8d2ef67a779d14996d6860c128cec6e9a
corey gibson <cgibsonmm@gmail.com>
2021-12-28 10:59:07 -0500

    tailwind config

---
+++ src/components/shared/Navbar.svelte
@@ -0,0 +1,3 @@
+<div>
+	<h1 class="bg-red-200 h-20">Budd Road Family Farm</h1>
+</div>
---
+++ src/routes/__layout.svelte
@@ -0,0 +1,14 @@
+<script>
+	import Navbar from '../components/shared/Navbar.svelte';
+</script>
+
+<div>
+	<Navbar />
+	<slot />
+</div>
+
+<style global>
+	@tailwind base;
+	@tailwind components;
+	@tailwind utilities;
+</style>
