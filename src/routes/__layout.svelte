c58b5ffa6e4c490aacd7ff19dd25aaffdbf504b3
corey gibson <cgibsonmm@gmail.com>
2021-12-28 10:59:07 -0500

    tailwind config

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,15 @@
+<script>
+	import Navbar from '../components/shared/Navbar.svelte';
+</script>
+
+<div>
+	<Navbar />
+	<slot />
+	Test
+</div>
+
+<style global>
+	@tailwind base;
+	@tailwind components;
+	@tailwind utilities;
+</style>
